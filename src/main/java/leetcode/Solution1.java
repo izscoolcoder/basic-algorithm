3461c92ce46bb7df147bf2dacf0d20b64737e955
zhushuai <zhucoolcoder@163.com>
2019-01-07 23:38:30 +0800

    leetcode

---
+++ src/main/java/leetcode/Solution1.java
@@ -0,0 +1,76 @@
+package leetcode;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Longest Substring Without Repeating Characters
+ *
+ * <p>Given a string, find the length of the longest substring without repeating characters.</p>
+ *
+ * <p>Example 1:
+ * <p>
+ * Input: "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * Example 2:
+ * <p>
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * Example 3:
+ * <p>
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Note that the answer must be a substring, "pwke" is a subsequence and not a substring.</p>
+ */
+public class Solution1 {
+
+
+    public static int lengthOfLongestSubstring(String s) {
+        int length = s.length();
+        int result = 0;
+        List<Character> temp = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            char character = s.charAt(i);
+            if (temp.contains(character)) {
+                result = Math.max(result, temp.size());
+                temp = temp.subList(temp.indexOf(character) + 1, temp.size());
+                temp.add(character);
+            } else {
+                temp.add(character);
+                result = Math.max(result, temp.size());
+            }
+        }
+        return result;
+    }
+
+
+    public static int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        String testStr = "abcdefgdab";
+        System.out.println(lengthOfLongestSubstring2(testStr));
+    }
+}
---
+++ src/main/java/leetcode/Solution2.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+public class Solution2 {
+
+    public static double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            // to ensure m<=n
+            int[] temp = A;
+            A = B;
+            B = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0;
+        int iMax = m;
+        int halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j - 1] > A[i]) {
+                iMin = i + 1;
+            } else if (i > iMin && A[i - 1] > B[j]) {
+                // i is too big
+                iMax = i - 1;
+            } else {
+                // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        int[] A = new int[]{1, 2,3,4,5,6,7};
+        int[] B = new int[]{-3, -2, 8, 9, 10, 11};
+
+        System.out.println(findMedianSortedArrays(A, B));
+
+    }
+
+
+}
