ad98b7175527e2dd47a0b8a20e1a965555dd0149
zhushuai <zhucoolcoder@163.com>
2019-01-14 23:30:38 +0800

    ThreadLocal 并不是解决线程之间共享变量的问题

---
+++ src/main/java/thread/ThreadLocalTest.java
@@ -0,0 +1,54 @@
+package thread;
+
+/**
+ * 验证一下threadLocal 是当前线程的  不能是其他线程之间共享变量
+ *
+ *
+ * @author zhushuai
+ */
+public class ThreadLocalTest {
+
+
+    ThreadLocal<Long> longLocal = new ThreadLocal<Long>();
+    ThreadLocal<String> stringLocal = new ThreadLocal<String>();
+
+
+    public void set() {
+        longLocal.set(Thread.currentThread().getId());
+        stringLocal.set(Thread.currentThread().getName());
+    }
+
+    public long getLong() {
+        return longLocal.get();
+    }
+
+    public String getString() {
+        return stringLocal.get();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final ThreadLocalTest test = new ThreadLocalTest();
+
+
+        test.set();
+        System.out.println(test.getLong());
+        System.out.println(test.getString());
+
+
+        Thread thread1 = new Thread(){
+            @Override
+            public void run() {
+                test.set();
+                System.out.println(test.getLong());
+                System.out.println(test.getString());
+            };
+        };
+        thread1.start();
+        thread1.join();
+
+        System.out.println(test.getLong());
+        System.out.println(test.getString());
+    }
+
+
+}
